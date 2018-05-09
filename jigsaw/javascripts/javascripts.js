63878bc0a5b7313baff5c35ee4b8af876e21f880
Courtney M <courtney92101@gmail.com>
2018-05-09 12:52:55 -0700

    Header 3 complete, Header 4 started

--- jigsaw/javascripts/javascripts.js
+++ jigsaw/javascripts/javascripts.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 
 });
 
+// Shadows, Elements, Luxe - Creates closing functionality for all off canvas close buttons
 function closeOffCanvas() {
 	var offCanvasMenu,
 		offCanvasOverlay = $('.js-off-canvas-overlay');
@@ -20,5 +21,23 @@ function closeOffCanvas() {
 	$(offCanvasMenu).removeClass('is-open');
 	$(offCanvasMenu).attr('aria-hidden', 'true');
 	$(offCanvasOverlay).removeClass('is-visible is-closable');
+}
+
+// Luxe Only - Adds additional refocus functionality if login tab content isn't already active
+function openMiniBasketLogin() {
+	var loginTab = $('.login-tab');
+
+	if (!$(loginTab).hasClass('is-active')) {
+		$(loginTab).click();
+	}
+}
+
+// Luxe Only - Adds additional refocus functionality if mini basket tab content isn't already active
+function openMiniBasketShopping() {
+	var shoppingTab = $('.shopping-tab');
+
+	if (!$(shoppingTab).hasClass('is-active')) {
+		$(shoppingTab).click();
+	}
 }
 
